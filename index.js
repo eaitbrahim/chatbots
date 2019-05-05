973048c058cf8c34e8306ea7cc5515665d4e43f1
Elmehdi <eaitbrahim@gmail.com>
2019-05-05 18:26:11 +0000

    2

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 require('./routes/jobRoutes')(app);
 require('./routes/candidatureRoutes')(app);
 

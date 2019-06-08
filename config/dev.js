1c20b26a1abdfefc7fff311d0d4ba260eb167a68
eaitbrahim <mister_elmehdi@hotmail.com>
2019-06-08 22:40:19 +0000

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  jobsSheetId: '1BwuA-wWCL3Uqe4mDe8L9ZyplwuRzM_ueBrlkh6VnVgY',
-  candidaturesSheetId: '1Ee8fKExMmEcAPDmvPl7mB7N_HtuaPepp1d6DHWVvOAc',
-  ticketSheetId: '1bnbvqNEBdf0TUzkuyZEhzPojijy88tsW6S6O34xaRPo',
-  manageEngineAuthToken: 'c99a06c7635b824e3efd065ae3e2e263',
-  manageEngineUrl: 'https://sdpondemand.manageengine.com/api/v3/requests',
-  twilioAccountSid: 'ACe8e6660ba4484d67def8623b89f6d35a',
-  twilioAuthToken: 'eddd8c8049da0a60cb7b38bc10e22e3d'
-};

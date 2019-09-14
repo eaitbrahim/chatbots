cffc84e825665b86aea458da44a753fbd4e1dd00
Elmehdi <eaitbrahim@gmail.com>
2019-09-14 09:56:49 +0100

    Initiate the validation

---
+++ routes/mesdp_onprem.js
@@ -0,0 +1,43 @@
+const _ = require('lodash');
+const {
+    stripIndent
+} = require('common-tags');
+const axios = require('axios');
+const keys = require('../config/keys');
+const ManageEngineLog = require('../services/manageEngineLogs');
+const jwt = require('jsonwebtoken');
+const authClientRequest = require('../middlewares/authGuard');
+
+module.exports = app => {
+    app.post('/api/me_requests', authClientRequest.authClientToken, async (req, res) => {
+        console.log('Received body:', req.body);
+        var result = {};
+        if (typeof req.body.originalDetectIntentRequest !== 'undefined') {
+            result = await processWhatsappData(req.body);
+        } else {
+            result = await processTicketChanges(req.body);
+        }
+        res.json(result);
+    });
+
+    app.get('/api/me_requests', authClientRequest.authClientToken, async (req, res) => {
+        res.json({
+            'Message': 'Hello!'
+        });
+    });
+
+    app.get('/api/me_generateJwt', async (req, res) => {
+        let token = jwt.sign({
+            id: keys.twilioSidForApi
+        }, keys.secret, {
+            expiresIn: 2592000
+        });
+        console.log('Token:', token);
+        res.status(200).json({
+            "success": [{
+                "msg": "Token generated successfully",
+                "token": token
+            }]
+        })
+    });
+};
